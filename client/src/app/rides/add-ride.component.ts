8fefc7495a6ad6ec28fd9f66da9a5a9ff52a378b
JReuvers <reuve026@morris.umn.edu>
2019-03-28 20:29:41 -0500

    Adds validation and the input field for mpg in add ride.    Issue #16

--- client/src/app/rides/add-ride.component.ts
+++ client/src/app/rides/add-ride.component.ts
@@ -41,6 +41,11 @@ export class AddRideComponent implements OnInit {
       {type: 'minlength', message: 'Departure Time must be at least 2 characters long'},
       {type: 'maxlength', message: 'Departure Time cannot be more than 100 characters long'},
     ],
+    'mpg': [
+      {type: 'min', message: 'MPG is too low, not reasonable'},
+      {type: 'max', message: 'MPG is too high, are you crazy!?'},
+      {type: 'pattern', message: 'MPG must be a number'}
+    ],
     'notes': [
       {type: 'required', message: 'Notes is required'},
       {type: 'minlength', message: 'Notes must be at least 2 characters long'},
@@ -74,6 +79,11 @@ export class AddRideComponent implements OnInit {
         Validators.minLength(2),
         Validators.maxLength(100),
         Validators.required
+      ])),
+      mpg: new FormControl('mpg', Validators.compose([
+        Validators.min(1),
+        Validators.max(200),
+        Validators.pattern('\\d')
       ])),
       notes: new FormControl('notes', Validators.compose([
         Validators.minLength(2),
