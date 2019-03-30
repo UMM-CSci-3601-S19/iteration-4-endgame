8210a451f406dafcc0c0423465e0fb7cef1ba240
Adam Schug <schug047@morris.umn.edu>
2019-03-29 20:39:21 -0500

    Removes requirement of the notes field when adding a ride.  This is a general fix.

--- client/src/app/rides/add-ride.component.ts
+++ client/src/app/rides/add-ride.component.ts
@@ -47,7 +47,6 @@ export class AddRideComponent implements OnInit {
       {type: 'pattern', message: 'MPG must be a number'}
     ],
     'notes': [
-      {type: 'required', message: 'Notes is required'},
       {type: 'minlength', message: 'Notes must be at least 2 characters long'},
       {type: 'maxlength', message: 'Notes cannot be more than 150 characters long'},
       {type: 'pattern', message: 'notes must contain only english and certain symbols'}
@@ -88,7 +87,6 @@ export class AddRideComponent implements OnInit {
       notes: new FormControl('notes', Validators.compose([
         Validators.minLength(2),
         Validators.maxLength(150),
-        Validators.required,
         Validators.pattern('^[?\'"></!@#$%^&*()_+= a-zA-Z0-9:._-]+$')
       ]))
 
