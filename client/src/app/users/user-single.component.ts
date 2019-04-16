203b9d8b388683d4b478d4ca9c1c45a105dbe09f
JReuvers <reuve026@morris.umn.edu>
2019-04-16 14:30:21 -0500

    Fixes testing issues e2e with the bug for deleting a ride    Issue #25

--- client/src/app/users/user-single.component.ts
+++ client/src/app/users/user-single.component.ts
@@ -14,19 +14,21 @@ import {Observable} from "rxjs/Observable";
 export class UserSingleComponent implements OnInit {
 
   user;
+  rating;
 
   constructor(private route: ActivatedRoute, private userService: UserService, public dialog: MatDialog) {
 
   }
 
   //TODO: needs to work for an individual file, as of right now it will pull an individual user, but can't interact or update it
-  editUserReviewDialog(currentId: string, currentName: string, currentEmail: string, currentPhoneNumber: string, reviewScore: number, rating: string, numReviews: number): void {
+  editUserReviewDialog(currentId: string, currentName: string, currentEmail: string, currentPhoneNumber: string, currentReviewScore: number, rating: string, numReviews: number): void {
     let newRating: number = parseInt(rating);
-    console.log("Old Rating: " + reviewScore + "  Number of Reviews: " + numReviews + "  New Rating: " + newRating);
-    if (reviewScore == null) {
-      reviewScore = newRating;
+    console.log("Old Rating: " + currentReviewScore + "  Number of Reviews: " + numReviews + "  New Rating: " + newRating);
+    if (currentReviewScore == null) {
+      console.log("The new Rating " + newRating + " is the review Score " + currentReviewScore);
+      currentReviewScore = newRating;
     } else {
-      reviewScore = reviewScore + newRating;
+      currentReviewScore = currentReviewScore + newRating;
     }
 
     const currentUser: User = {
@@ -36,8 +38,8 @@ export class UserSingleComponent implements OnInit {
       name: currentName,
       email: currentEmail,
       phoneNumber: currentPhoneNumber,
-      // Tests work when removing the s in the reviewScores changing this to reviewScore.  However, doing this causes the review system to break.
-      reviewScores: reviewScore,
+      // Tests work when removing the s in the currentReviewScore changing this to currentReviewScore.  However, doing this causes the review system to break.
+      reviewScores: currentReviewScore,
       numReviews: numReviews + 1 || 1
     };
 
@@ -64,22 +66,25 @@ export class UserSingleComponent implements OnInit {
     });
   }
 
-  refreshUser(): Observable<User[]> {
+  refreshUser(): void {
     // Get Users returns an Observable, basically a "promise" that
     // we will get the data from the server.
     //
     // Subscribe waits until the data is fully downloaded, then
     // performs an action on it (the first lambda)
 
-    const user: Observable<User[]> = this.userService.getUsers();
-    user.subscribe(
-      user => {
-        this.user = user;
-      },
-      err => {
-        console.log(err);
-      });
-    return user;
+    this.route.params.subscribe(params => {
+      const userId = params['userId'];
+      const user: Observable<User[]> = this.userService.getUsers(userId);
+      user.subscribe(
+        user => {
+          this.user = user;
+        },
+        err => {
+          console.log(err);
+        });
+      return user;
+    });
   }
 
   ngOnInit(): void {
