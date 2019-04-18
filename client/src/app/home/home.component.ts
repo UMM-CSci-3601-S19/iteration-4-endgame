e916f785df7d0e72b2f849fd612fb68e6cc0dd6a
walte774 <walte774@morris.umn.edu>
2019-04-18 14:11:01 -0500

    Adds client-side sign-in and sign-out
      Loads gapi and declares auth2 in index.html
      Adds signIn and signOut functions in home.component.ts
      Adds sign in and sign out buttons in home.component.html

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 
+//Declare pulls the variable from the html/js environment, so our gapi we declared in index gets pulled here.
+declare let auth2: any;
+
 @Component({
   templateUrl: 'home.component.html',
   styleUrls: ['./home.component.css']
@@ -9,5 +12,15 @@ export class HomeComponent {
 
   constructor() {
     this.text = 'MoRide';
+  }
+
+  signIn() {
+    console.log("Signing in!");
+    auth2.signIn();
+  }
+
+  signOut() {
+    console.log("Signing out!");
+    auth2.signOut();
   }
 }
