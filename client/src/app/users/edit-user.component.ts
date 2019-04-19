ad4b67d6176327db3d250a4b93622fcbaea39cb2
JReuvers <reuve026@morris.umn.edu>
2019-04-18 22:26:07 -0500

    Sets up functional editing of the bio and phone number     Issue #11     Issue #13     Issue #14

--- client/src/app/users/edit-user.component.ts
+++ client/src/app/users/edit-user.component.ts
@@ -12,11 +12,31 @@ import {FormControl, Validators, FormGroup, FormBuilder} from "@angular/forms";
 export class EditUserComponent implements OnInit {
   currentId: string;
 
+  editInfoForm: FormGroup;
+
   public user: User[];
 
   constructor(
-    @Inject(MAT_DIALOG_DATA) public data: any, private fb: FormBuilder) {
+    @Inject(MAT_DIALOG_DATA) public data: {user: User}, private fb: FormBuilder) {
   }
+
+  edit_info_validation_messages = {
+    'bio': [
+
+    ],
+    'phoneNumber': [
+
+    ]
+  };
+
+  createForms() {
+    this.editInfoForm = this.fb.group({
+      bio: new FormControl('bio', Validators.compose([])),
+      phoneNumber: new FormControl('phoneNumber', Validators.compose([]))
+    })
+  }
+
   ngOnInit(): void {
+    this.createForms();
   }
 }
--- client/src/app/users/user-profile.component.ts
+++ client/src/app/users/user-profile.component.ts
@@ -9,60 +9,34 @@ import {ActivatedRoute} from "@angular/router";
 @Component({
   selector: 'user-profile-component',
   templateUrl: 'user-profile.component.html',
-  styleUrls: ['./user-profile.component.css'],
+  styleUrls: ['./user-profile.component.css']
 })
 
 export class UserProfileComponent implements OnInit {
 
-  //user/*: User[]*/;
   user: User;
-  rating;
-
-  public selected: string;
 
   // Inject the UserListService into this component.
   constructor(private route: ActivatedRoute, public userService: UserService, public dialog: MatDialog) {
 
   }
 
-  // public filterUsers(searchOid: string): User[] {
-  //
-  //   this.filteredUsers = this.users;
-  //
-  //   if (searchOid != null) {
-  //     searchOid = searchOid.toLocaleLowerCase();
-  //
-  //     this.filteredUsers = this.filteredUsers.filter(user => {
-  //       return !searchOid || user._id.$oid.indexOf(searchOid) !== -1;
-  //     });
-  //   }
-  //
-  //   return this.filteredUsers;
-  // }
-
-  editUserReviewDialog(currentId: string, currentName: string, currentEmail: string, currentPhoneNumber: string, reviewScore: number, rating: string, numReviews: number): void {
-    let newRating: number = parseInt(rating);
-    console.log("Old Rating: " + reviewScore + "  Number of Reviews: " + numReviews + "  New Rating: " + newRating);
-    if (reviewScore == null) {
-      reviewScore = newRating;
-    } else {
-      reviewScore = reviewScore + newRating;
-    }
-
-    const currentUser: User = {
+  editUserDialog(currentId: string, currentName: string, currentBio: string,currentPhoneNumber: string,
+                 currentEmail: string, currentTotalReviewScore: number, currentNumReviews: number): void {
+    const currentInfo: User = {
       _id: {
         $oid: currentId
       },
       name: currentName,
-      email: currentEmail,
+      bio: currentBio,
       phoneNumber: currentPhoneNumber,
-      // Tests work when removing the s in the reviewScore changing this to reviewScore.  However, doing this causes the review system to break.
-      reviewScores: reviewScore,
-      numReviews: numReviews + 1 || 1
+      email: currentEmail,
+      totalReviewScore: currentTotalReviewScore,
+      numReviews: currentNumReviews
     };
 
     const dialogConfig = new MatDialogConfig();
-    dialogConfig.data = {user: currentUser};
+    dialogConfig.data = {user: currentInfo};
     dialogConfig.width = '500px';
 
     const dialogRef = this.dialog.open(EditUserComponent, dialogConfig);
@@ -73,7 +47,7 @@ export class UserProfileComponent implements OnInit {
         this.userService.editUser(currentUser).subscribe(
           result => {
             console.log("The result is " + result);
-            this.ngOnInit();
+            this.refreshUser();
           },
           err => {
             console.log('There was an error editing the ride.');
@@ -84,45 +58,73 @@ export class UserProfileComponent implements OnInit {
     });
   }
 
-  refreshUser(): void/*Observable<User[]>*/ {
-    // Get Users returns an Observable, basically a "promise" that
-    // we will get the data from the server.
-    //
-    // Subscribe waits until the data is fully downloaded, then
-    // performs an action on it (the first lambda)
-
-    this.route.params.subscribe(params => {
-      const userId = params['userId'];
-      const user: Observable<User> = this.userService.getUserById(userId);
-      user.subscribe(
-        user => {
-          this.user = user;
-        },
-        err => {
-          console.log(err);
-        });
-      return user;
-    });
-  }
-
-  // loadService(): void {
-  //   this.userService.getUsers().subscribe(
-  //     users => {
-  //       this.users = users;
-  //       this.filteredUsers = this.users;
+  // editUserReviewDialog(currentId: string, currentName: string, currentBio: string, currentEmail: string, currentPhoneNumber: string, reviewScore: number, rating: string, numReviews: number): void {
+  //   let newRating: number = parseInt(rating);
+  //   console.log("Old Rating: " + reviewScore + "  Number of Reviews: " + numReviews + "  New Rating: " + newRating);
+  //   if (reviewScore == null) {
+  //     reviewScore = newRating;
+  //   } else {
+  //     reviewScore = reviewScore + newRating;
+  //   }
+  //
+  //   const currentUser: User = {
+  //     _id: {
+  //       $oid: currentId
   //     },
-  //     err => {
-  //       console.log(err);
+  //     name: currentName,
+  //     bio: currentBio,
+  //     email: currentEmail,
+  //     phoneNumber: currentPhoneNumber,
+  //     // Tests work when removing the s in the reviewScore changing this to reviewScore.  However, doing this causes the review system to break.
+  //     reviewScores: reviewScore,
+  //     numReviews: numReviews + 1 || 1
+  //   };
+  //
+  //   const dialogConfig = new MatDialogConfig();
+  //   dialogConfig.data = {user: currentUser};
+  //   dialogConfig.width = '500px';
+  //
+  //   const dialogRef = this.dialog.open(EditUserComponent, dialogConfig);
+  //
+  //   console.log("Dialog Ref " + dialogRef.toString());
+  //   dialogRef.afterClosed().subscribe(currentUser => {
+  //     if (currentUser != null) {
+  //       this.userService.editUser(currentUser).subscribe(
+  //         result => {
+  //           console.log("The result is " + result);
+  //           this.ngOnInit();
+  //         },
+  //         err => {
+  //           console.log('There was an error editing the ride.');
+  //           console.log('The currentRide or dialogResult was ' + JSON.stringify(currentUser));
+  //           console.log('The error was ' + JSON.stringify(err));
+  //         });
   //     }
-  //   );
+  //   });
   // }
 
-  ngOnInit(): void {
+  refreshUser(): void {
     this.route.params.subscribe(params => {
       const userId = params['userId'];
       this.userService
         .getUserById(userId)
-        .subscribe(user => this.user = user);
+        .subscribe(subscribedUser => {
+          console.log(JSON.stringify(subscribedUser));
+          this.user = subscribedUser
+        });
     });
+  }
+
+  ngOnInit(): void {
+    this.refreshUser();
+    // this.route.params.subscribe(params => {
+    //   const userId = params['userId'];
+    //   this.userService
+    //     .getUserById(userId)
+    //     .subscribe(user => {
+    //       console.log(JSON.stringify(user));
+    //       this.user = user
+    //     });
+    // });
   }
 }
