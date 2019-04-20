79f061f7532a98580b003bf9846b3ec7fdec616e
Adam Schug <schug047@morris.umn.edu>
2019-04-20 00:59:56 -0500

    Changes types to be primitive string instead of String

--- client/src/app/users/user.ts
+++ client/src/app/users/user.ts
@@ -2,10 +2,10 @@ export interface User {
   _id: {
     $oid: string;
   };
-  name: String;
-  bio: String;
-  email: String;
-  phoneNumber?: String;
+  name: string;
+  bio: string;
+  email: string;
+  phoneNumber?: string;
   totalReviewScore?: number;
   numReviews?: number;
   avgScore?: number;
